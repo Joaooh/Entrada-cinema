6535020ecd8a0c207ad0b1985a6de7f9f671704d
João Gabriel <83705635+Joaooh@users.noreply.github.com>
2025-05-07 15:57:04 -0300

    Update entrada-cinema.py
    Code completely rewritten for better structure and clarity.

--- entrada-cinema.py
+++ entrada-cinema.py
@@ -1,44 +1,31 @@
 def linhas():
     print("-" * 28)
 
-# Fica perguntando a idade pro usuário se não digitar um valor inteiro.
-linhas()
 while True:
-    try:
-        idade = int(input("Qual sua idade? "))
-        if isinstance (idade, int):
-            break
-        else:
-            idade = int(input("Qual sua idade? "))
-    except ValueError:
+    linhas()
+    idade = int(input("Qual sua idade? "))
+    while idade <= 0:
         print("Entrada inválida.")
+        idade = int(input("Qual sua idade? "))
 
-# Pergunta se tem ingresso até que digite S ou N.
-ingresso = str(input("Você tem ingresso? (S/N): ")).lower()
-while ingresso not in "sn":
-    print("Digite S para sim, N para não.")
-    ingresso = str(input("Você tem ingresso? (S/N): ")).lower()
+    ingresso = str(input("Você tem ingresso? (S/N): ").lower())
+    while ingresso[:1] != "s" and ingresso[:1] != "n":
+        ingresso = str(input("Você tem ingresso? (S/N): ").lower())
 
-# Verificação final se pode ou não entrar.
-while True:
-    try:
-        if ingresso == "n":
-            ingresso = False
-        else:
-            ingresso = True
+    if ingresso == "s":
+        ingresso = True
+    else:
+        ingresso = False
 
-        if (idade >= 16) and (ingresso == True):
-            print("Pode entrar")
-        else:
-            print("Não pode entrar")
-        linhas()
-        # Pergunta se quer continuar o programa. Se sim, reinicia o código.
-        continuar = str(input("Verificar novamente? (S/N): ")).lower()
-        while continuar not in "sn":
-            print("Digite S se tem, N se não tem.")
-            continuar = str(input("Verificar novamente? (S/N): ")).lower()
-        if continuar == "n":
-            linhas()
-            break
-    except ValueError:
-        print("Erro: Entrada inválida.")
+    if (idade >= 16) and (ingresso == True):
+        print("\nPode entrar!")
+    else:
+        print("\nNão pode entrar.")
+    linhas()
+
+    continuar = str(input("Verificar novamente? (S/N): ").lower())
+    while continuar[:1] != "s" and continuar[:1] != "n":
+        continuar = str(input("Verificar novamente? (S/N): ").lower())
+
+    if continuar == "n":
+        break
